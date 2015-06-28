777c6363c9e2bc8106d65e9aed93cdf2a70db95f
Pavlo Voznenko <p.voznenko@gmail.com>
2015-06-28 23:12:19 +0200

    paths refactoring

--- src/main/scala/InstagramPicsFilter/Routes.scala
+++ src/main/scala/InstagramPicsFilter/Routes.scala
@@ -3,29 +3,16 @@ package InstagramPicsFilter
 import akka.http.scaladsl.server.Directives._
 
 object Routes {
-
   val paths = logRequestResult("akka-http-microservice") {
-    path("diagnostics" / "heartbeat") {
-      get {
-        complete {
-          "Ok!"
-        }
-      }
+    (path("diagnostics" / "heartbeat") & get) {
+      complete("Ok!")
     } ~
-      path("index" / LongNumber) { shardSequenceId =>
-        get {
-          complete(
-            "Indexing in progress..."
-          )
-        }
-      } ~
-      path("index" / LongNumber / LongNumber / IntNumber) { (start, end, parallelism) =>
-        get {
-          complete(
-            "Indexing in progress..."
-          )
-        }
-      } ~
-      path("")(getFromResource("public/index.html"))
+    (path("index" / LongNumber) & get) { shardSequenceId =>
+      complete("Indexing in progress...")
+    } ~
+    (path("index" / LongNumber / LongNumber / IntNumber) & get) { (start, end, parallelism) =>
+      complete("Indexing in progress...")
+    } ~
+    path("")(getFromResource("public/index.html"))
   }
 }
