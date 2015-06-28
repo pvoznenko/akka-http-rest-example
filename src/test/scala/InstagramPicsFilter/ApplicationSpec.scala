acb000e7fa155c3657f11497c08fa7cb31f7ee40
Pavlo Voznenko <p.voznenko@gmail.com>
2015-06-28 22:57:24 +0200

    used akka.http.scaladsl.testkit for tests

--- src/test/scala/InstagramPicsFilter/ApplicationSpec.scala
+++ src/test/scala/InstagramPicsFilter/ApplicationSpec.scala
@@ -1,48 +1,30 @@
 package InstagramPicsFilter
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{StatusCodes, HttpRequest}
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.scaladsl.{Sink, Source}
-import org.scalatest.concurrent.PatienceConfiguration.Timeout
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.time.{Second, Milliseconds, Span, Seconds}
-import org.scalatest.{BeforeAndAfterAll, Matchers, FlatSpec}
-import akka.stream.ActorMaterializer
-import scala.concurrent.ExecutionContext.Implicits.global
+import org.scalatest.{ Matchers, WordSpec }
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.server._
+import Directives._
 
-class ApplicationSpec extends FlatSpec with Matchers with ScalaFutures with BeforeAndAfterAll with Server {
-  implicit val testSystem = ActorSystem("instagram-pics-filter-test")
-  implicit val testMaterializer = ActorMaterializer()
+class ApplicationSpec extends WordSpec with Matchers with Server with ScalatestRouteTest {
+  val routes = Routes.paths
 
-  override def beforeAll() = start()
-
-  override def afterAll() = testSystem.shutdown()
-
-  def sendRequest(req: HttpRequest) =
-    Source.single(req).via(
-      Http().outgoingConnection(host = config.getString("http.host"), port = config.getInt("http.port"))
-    ).runWith(Sink.head)
-
-  "The app" should "return index.html on a GET to /" in {
-    val request = sendRequest(HttpRequest())
-
-    whenReady(request, Timeout(Span(1, Second))) { response =>
-      response.status shouldBe StatusCodes.OK
-
-      val stringFuture = Unmarshal(response.entity).to[String]
-
-      whenReady(stringFuture) { str =>
-        str should include("Hello World!")
-      }
+  "return a greeting for GET requests to the root path" in {
+    Get() ~> routes ~> check {
+      responseAs[String] should include("Hello World!")
     }
   }
-  "The app" should "return 404 on a GET to /foo" in {
-    val request = sendRequest(HttpRequest(uri = "/foo"))
 
-    whenReady(request) { response =>
-      response.status shouldBe StatusCodes.NotFound
+  "leave GET requests to other paths unhandled" in {
+    Get("/kermit") ~> routes ~> check {
+      handled shouldBe false
+    }
+  }
+
+  "return a MethodNotAllowed error for PUT requests to the root path" in {
+    Put() ~> Route.seal(routes) ~> check {
+      status === StatusCodes.MethodNotAllowed
+      responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
     }
   }
 }
